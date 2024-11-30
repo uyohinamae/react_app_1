abb0976790a90450b7a7902d7c0b82e743004f5a
supu <zmaee90909@yahoo.co.jp>
2024-11-30 18:44:35 +0900

    dom

---
+++ 03_js_basic/045_eventlistener/start/main.js
@@ -0,0 +1,10 @@
+const h1Element = document.querySelector('h1')
+console.dir(h1Element)
+console.log(h1Element.taxtContent)
+h1Element.textContent = '変更後のタイトル'
+
+const btnEl = document.querySelector('button');
+btnEl.addEventListener('click' , (e) => {
+    console.log(e.target.taxtContent);
+    console.log('hello')
+})
