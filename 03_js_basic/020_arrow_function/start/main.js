7981361aa789d8f3594a0526716751b9f298739f
supu <zmaee90909@yahoo.co.jp>
2024-11-30 17:27:53 +0900

    module

--- 03_js_basic/020_arrow_function/start/main.js
+++ 03_js_basic/020_arrow_function/start/main.js
@@ -2,6 +2,12 @@ function fn(number) {
   return number * 2;
 }
 
+console.log(fn(2));
+
 const fnArrow = (number) => {
+  console.log(number);
   return number * 2;
 }
+
+const fnArrowobj = number => ({ result: number * 2 })
+console.log(fnArrow(2));
---
+++ 03_js_basic/030_esmodule/start/main.js
@@ -0,0 +1,6 @@
+import functionB, { hello, User } from "./module.js";
+
+hello();
+functionB()
+const user = new User('Tom');
+user.hello();
