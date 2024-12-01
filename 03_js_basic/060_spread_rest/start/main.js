d83294cc13c5bd71afad78b9dce32b39468ce064
supu <zmaee90909@yahoo.co.jp>
2024-12-01 18:09:36 +0900

    react

--- 03_js_basic/060_spread_rest/start/main.js
+++ 03_js_basic/060_spread_rest/start/main.js
@@ -21,8 +21,8 @@ newObj.name = 'john';
 
 console.log(newObj, obj);
 
-// const restA = (...argA) => console.log(argA);
-// restA(1, 3, 4)
+const restA = (...argA) => console.log(argA);
+restA(1, 3)
 
-// const restB = (n, ...argB) => console.log(argB);
-// restB(1, 3, 4)
+const restB = (n, ...argB) => console.log(argB);
+restB(1, 3, 4)
